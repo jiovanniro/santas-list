77fc39ac2a03f29626c73d96f08e29f0e44debda
JohnLea83 <j.r.lea83@gmail.com>
2017-12-06 09:57:20 -0600

    fixed home

--- client/app.js
+++ client/app.js
@@ -11,23 +11,23 @@ $routeProvider
 
     .when('/thankyou', {
         templateUrl: 'views/thankyou.html',
-        controller: 'productsController'
-        // requiresLogin: true
+        controller: 'productsController',
+        requiresLogin: true
     })
     .when('/adultSignUp', {
-        templateUrl: 'views/adult_signup.html'
-        // controller: 'LoginController',
+        templateUrl: 'views/adult_signup.html',
+        controller: 'LoginController'
     })
     
     .when('/adultSignIn', {
-        templateUrl: 'views/adult_signin.html'
-        // controller: 'LoginController'
+        templateUrl: 'views/adult_signin.html',
+        controller: 'LoginController'
     })
 
     .when('/kidSignUp', {
         templateUrl: 'views/kid_signup.html',
         controller: 'ChildLoginController'
-        //This might need requiresAdmin and requiresLogin
+        // This might need requiresAdmin and requiresLogin
     })
     
     .when('/kidSignIn', {
@@ -37,31 +37,30 @@ $routeProvider
     
     .when('/kid', {
         templateUrl: 'views/kid.html',
-        controller: 'ChildController'
-        // requiresLogin: true
+        controller: 'ChildController',
+        requiresLogin: true
     })
     
     .when('/adult', {
         templateUrl: 'views/adult.html',
-        controller: 'AdultController'
-        // requiresLogin: true,
-        // requiresAdmin: true
+        controller: 'AdultController',
+        requiresLogin: true,
+        requiresAdmin: true
+    })
+    
+    .otherwise({
+        redirectTo: '/'
     });
-
-
-    // .otherwise({
-    //     redirectTo: '/'
-    // });
-}]);
+}])
     // * set for redirect path. This might be editted out later on. *
-// .run(['$rootScope', '$location', 'UserService', function($rootScope, $location, UserService){
-//     $rootScope.$on('$rootChangeStart', function(event, nextRoute, previousRoute){
-//         if (nextRoute.$$route.requiresLogin && !UserService.isLoggedIn()) {
-//             event.preventDefault();
-//             UserService.loginRedirect();
-//         } else if (nextRoute.$$route.requiresAdmin && !UserService.isAdmin()){
-//             event.preventDefault();
-//             $location.replace().path('/adult');
-//         }
-//     })
-// }])
+.run(['$rootScope', '$location', 'UserService', function($rootScope, $location, UserService){
+    $rootScope.$on('$rootChangeStart', function(event, nextRoute, previousRoute){
+        if (nextRoute.$$route.requiresLogin && !UserService.isLoggedIn()) {
+            event.preventDefault();
+            UserService.loginRedirect();
+        } else if (nextRoute.$$route.requiresAdmin && !UserService.isAdmin()){
+            event.preventDefault();
+            $location.replace().path('/adult');
+        }
+    });
+}]);
