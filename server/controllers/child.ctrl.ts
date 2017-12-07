8383eb34c2b481987d3d10e3be974d23564fb12c
Jiovanni Rosario <jiovanni.ro@gmail.com>
2017-12-07 16:17:58 -0600

    Reordered login in routes

--- server/controllers/child.ctrl.ts
+++ server/controllers/child.ctrl.ts
@@ -6,6 +6,29 @@ import * as utils from '../utils';
 
 const router = express.Router();
 
+router.post('/login', (req, res, next) => {
+    console.log("inside of child ctrl login");
+    console.log(req.body);
+
+    //authenticating the request
+    passport.authenticate('local-child', (err: any, user: models.IUser, info: any) => {
+        if (err) {
+            console.log(err); 
+            return res.sendStatus(500);
+        } 
+        if (!user) {
+            return res.status(401).send(info);//info is message sent from passport.ts
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return res.sendStatus(500);
+            } else {
+                return res.send(user);//send user to passport to be serialized or deserialized
+            }
+        });
+    })(req, res, next);
+});
+
 //might need to change location if it does not work
 router.post('/createChild', function(req, res){
     utils.encryptPassword(req.body.password)
@@ -48,27 +71,6 @@ router.get('/:id', function(req, res){
     });
 })
 
-router.post('/login', (req, res, next) => {
-    console.log("inside of child ctrl login");
-    console.log(req.body);
 
-    //authenticating the request
-    passport.authenticate('local-child', (err: any, user: models.IUser, info: any) => {
-        if (err) {
-            console.log(err); 
-            return res.sendStatus(500);
-        } 
-        if (!user) {
-            return res.status(401).send(info);//info is message sent from passport.ts
-        }
-        req.logIn(user, (err) => {
-            if (err) {
-                return res.sendStatus(500);
-            } else {
-                return res.send(user);//send user to passport to be serialized or deserialized
-            }
-        });
-    })(req, res, next);
-});
 
 export default router;
